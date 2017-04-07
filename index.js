2f6db645b1b0aa7fdda956dc46b89474a723c192
Johnnie Tirado <tiradoyamin@gmail.com>
2017-04-07 09:00:14 -0400

    Prueba Parkner

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+* Prueba de programación de Parkner
+* Debes de llenar de abajo
+* y pasar los test en test.js
+*
+*/
+
+/*
+* El problema:
+* Quiero saber cuantas veces ocurre la misma combinacion de letra en un string dentro de un array.
+* Ejemplo: Array -> [ 'ab', 'ba' ].
+* En este caso la combinacion 'a' y 'b' ocurre 2 veces en 2 strings diferentes.
+* Debes regresar un Array, en order de mayor ocurrencia a menor.
+* En este caso regresaria -> [ 2 ];
+* 
+* Entra /test/test.js si quieres ver el valor de diccionario.
+*
+* Cualquier pregunta, mandar un email a: johnnie.tirado@parkner.com.pe
+*
+*/
+
+var Cuenta = function(diccionario){
+    console.log('\nImplementa aqui tu logica!\n');
+    return [];
+}
+
+module.exports = Cuenta;
